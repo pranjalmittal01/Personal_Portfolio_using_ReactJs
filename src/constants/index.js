553249bce26da53b54a0f3d4add4169e61632b5f
pranjalmittal01 <pranjalmittal810@gmail.com>
2024-07-20 19:14:45 +0530

    Complete Personal Portfolio using React JS

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+// project images import
+import project1 from "../assets/projects_pictures/project-1.png";
+import project2 from "../assets/projects_pictures/project-2.png";
+import project3 from "../assets/projects_pictures/project-3.png";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I am a fresher, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL and MongoDB. My goal is to leverage my expertise to create innovative solutions that increase growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including ReactJS, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+    {
+        duration: "July 2023 - August 2023",
+        role: "PHP Developer",
+        company: "GENX Soft Technologies Pvt. Ltd.",
+        description: `I was working as a intern in Genx Soft Technologies. Where I learned many more things and worked under the seniors and also i worked on some live projects of the company by using PHP. And I learned new technologies related to the full stack development.`,
+        technologies: ["PHP", "Javascript", "React.js", "mongoDB"],
+    }
+];
+
+export const PROJECTS = [
+    {
+        title: "Student Management System",
+        image: project1,
+        description:
+            "A fully functional student management website with features like student details, document uploading and admin authentication. All the details of students is store into the database.",
+        technologies: ["HTML", "CSS", "BootStrap", "JavaScript", "PHP", "MySQL"],
+    },
+    {
+        title: "Text Converter",
+        image: project2,
+        description:
+            "A website for managing text. We can modify our text according to need we can apply many operation on the text as well as we can count our words and how much time user will take to read the text.",
+        technologies: ["HTML", "CSS", "ReactJs", "BootStrap"],
+    },
+    {
+        title: "NEWS Buzz Website",
+        image: project3,
+        description:
+            "A NEWS website which shows the latest news with the name of author, source and date. It will fetch the news fron NEWSAPI.",
+        technologies: ["HTML", "CSS", "React", "BootStrap", "JavaScript", "NEWSAPI"],
+    },
+];
+
+export const CONTACT = {
+    address: "71, Rodney Street, Wisconsin, US, 53703 ",
+    phoneNo: "608-282-3746 ",
+    email: "xyz@gmail.com",
+  };
